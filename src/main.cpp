3c136831e2f621b271af52952e40a1ee924bf419
cyril grinko <k.a.grinko@gmail.com>
2024-11-09 04:08:41 +0300

    first steps in creating ui

--- src/main.cpp
+++ src/main.cpp
@@ -1,6 +1,18 @@
 #include <SFML/Graphics.hpp>
 
 int main() {
-  sf::RenderWindow window(sf::VideoMode(800, 600), "My window");
+  sf::RenderWindow window(sf::VideoMode(800, 600), "Desmos");
+
+  while (window.isOpen()) {
+    sf::Event event;
+    if (window.pollEvent(event)) {
+      if (event.type == sf::Event::Closed) {
+        window.close();
+      }
+    }
+
+    window.display();
+    window.clear();
+  }
   return 0;
 }
