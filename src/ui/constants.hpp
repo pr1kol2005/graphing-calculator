99855b63caf05db51a8d3ad8df15eafcd6e718e1
cyril grinko <k.a.grinko@gmail.com>
2024-11-10 21:14:48 +0300

    created constants header

---
+++ src/ui/constants.hpp
@@ -0,0 +1,10 @@
+#pragma once
+
+#include <string>
+
+const int WINDOW_WIDTH = 800;
+const int WINDOW_HEIGHT = 600;
+const int WINDOW_CENTER_X = WINDOW_WIDTH / 2;
+const int WINDOW_CENTER_Y = WINDOW_HEIGHT / 2;
+
+const std::string WINDOW_TITLE = "My Desmos";
