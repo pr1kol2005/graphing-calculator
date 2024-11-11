4a09430cb2e1a3aa2ad4c1362f533c5f79645261
cyril grinko <k.a.grinko@gmail.com>
2024-11-11 17:47:18 +0300

    updated main

--- src/main.cpp
+++ src/main.cpp
@@ -1,5 +1,32 @@
 #include <SFML/Graphics.hpp>
 
+#include "canvas.hpp"
+#include "constants.hpp"
+#include "input_field.hpp"
+#include "bridge_controller.hpp"
+
 int main() {
+  sf::RenderWindow window(sf::VideoMode(WINDOW_WIDTH, WINDOW_HEIGHT),
+                          WINDOW_TITLE);
+
+  BridgeController controller(std::make_unique<InputField>(10, 10, 200, 50),
+                             std::make_unique<GraphImplementation>("1", WINDOW_WIDTH, 0.01),
+                             std::make_unique<Canvas>(WINDOW_WIDTH, WINDOW_HEIGHT));
+
+  while (window.isOpen()) {
+    sf::Event event;
+    while (window.pollEvent(event)) {
+      if (event.type == sf::Event::Closed) {
+        window.close();
+      }
+
+      controller.HandleEvent(event);
+    }
+
+    window.clear(sf::Color::White);
+
+    controller.Draw(window);
+    window.display();
+  }
   return 0;
 }
