4845afb35b27ac991b5135997c03fd86ae848085
cyril grinko <k.a.grinko@gmail.com>
2024-11-11 17:47:00 +0300

    updated constants

--- src/ui/constants.hpp
+++ src/ui/constants.hpp
@@ -7,4 +7,6 @@ const int WINDOW_HEIGHT = 800;
 const int WINDOW_CENTER_X = WINDOW_WIDTH / 2;
 const int WINDOW_CENTER_Y = WINDOW_HEIGHT / 2;
 
+const int GRID_SPACING = WINDOW_WIDTH / 10;
+
 const std::string WINDOW_TITLE = "My Desmos";
