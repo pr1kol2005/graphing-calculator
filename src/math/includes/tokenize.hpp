f66930e1007362a9940de1bcd0483cf2a1c9defd
cyril grinko <k.a.grinko@gmail.com>
2024-12-18 07:07:20 +0300

    removed and add tokens

--- src/math/includes/tokenize.hpp
+++ src/math/includes/tokenize.hpp
@@ -16,27 +16,41 @@ struct UnknownToken {
   explicit UnknownToken(std::string value_) : value(value_) {}
 };
 
+struct VariableToken {};
+
+struct OpeningBracketToken {};
+struct ClosingBracketToken {};
+
 struct PlusToken {};
 struct MinusToken {};
 struct MultiplyToken {};
 struct DivideToken {};
-struct MinToken {};
-struct MaxToken {};
-struct AbsToken {};
-struct ResidualToken {};
-struct OpeningBracketToken {};
-struct ClosingBracketToken {};
-struct VariableToken {};
 struct PowerToken {};
+
+struct AbsToken {};
 struct SqrtToken {};
 struct SinToken {};
 struct CosToken {};
 struct TanToken {};
+struct AsinToken {};
+struct AcosToken {};
+struct AtanToken {};
+struct SinhToken {};
+struct CoshToken {};
+struct TanhToken {};
+struct AsinhToken {};
+struct AcoshToken {};
+struct AtanhToken {};
+struct LnToken {};
+struct LgToken {};
+struct Log2Token {};
 
-using Token = std::variant<NumberToken, UnknownToken, PlusToken, MinusToken,
-                           MultiplyToken, DivideToken, MinToken, MaxToken,
-                           AbsToken, ResidualToken, OpeningBracketToken,
-                           ClosingBracketToken, VariableToken, PowerToken,
-                           SqrtToken, SinToken, CosToken, TanToken>;
+using Token =
+    std::variant<NumberToken, UnknownToken, VariableToken, OpeningBracketToken,
+                 ClosingBracketToken, PlusToken, MinusToken, MultiplyToken,
+                 DivideToken, PowerToken, AbsToken, SqrtToken, SinToken,
+                 CosToken, TanToken, AsinToken, AcosToken, AtanToken, SinhToken,
+                 CoshToken, TanhToken, AsinhToken, AcoshToken, AtanhToken,
+                 LnToken, LgToken, Log2Token>;
 
 std::vector<Token> Tokenize(std::string_view str);
